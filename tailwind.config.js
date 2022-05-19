2b35c67c8dfc1156d13a68b47b89e759344186a2
salikur6 <salikur6@gmail.com>
2022-05-19 19:27:25 +0600

    light mode and dark mode setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   daisyui: {
     themes: [
       {
-        mytheme: {
-          "primary": "#176F6B",
-          "secondary": "#FFC000",
-          "accent": "#FFEDD5",
-          "neutral": "#F3F4F6",
-          "base-100": "#FFFFFF",
-          "info": "#98A8DD",
-          "success": "#1BBB70",
-          "warning": "#DF7E07",
-          "error": "#FA5C5C",
+        light: {
+          primary: "#176F6B",
+          secondary: "#FFC000",
+          accent: "#FFEDD5",
+          neutral: "#F3F4F6",
+          "base-100": "#ffffff",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
         },
       },
+      "dark",
     ],
   },
   plugins: [require("daisyui")],
-}
+};
