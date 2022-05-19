28f5a1432c166dbd84ca1c82cea47847447971bf
salikur6 <salikur6@gmail.com>
2022-05-19 17:29:55 +0600

    navbar setup and other pages and routes setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Services from './pages/Services';
+import Navbar from './shared/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/services' element={<Services></Services>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
+        </Routes>
+
+
+
+
+
+      </Navbar>
 
     </div>
   );
